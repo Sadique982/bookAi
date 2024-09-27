ced56c2b6812a66dbcb9f3b7aae725f3284d9eb3
Mohammed Sadique <m.sadique04@icloud.com>
2024-09-27 05:57:09 +0530

    [Changes]

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,19 +1,19 @@
-import "./App.css";
-import React, { useRef } from "react";
-import { Canvas, useFrame } from "@react-three/fiber";
-import { Stars } from "@react-three/drei";
-import { motion } from "framer-motion";
-import Navbar from "./pages/Navbar/Navbar";
-import MainBody from "./pages/MainBody/MainBody";
-import AiDemo from "./pages/AiDemo/AiDemo";
-import Features from "./pages/Features/Features";
-import Footer from "./pages/Footer/Footer";
-import WorkRoadMap from "./pages/WorkRoadmap/WorkRoadMap";
-import BookSample from "./pages/BookSample/BookSample";
-import Testimonials from "./pages/Testimonials/Testimonials";
-import NewsLetter from "./pages/NewsLetter/NewsLetter";
-import Book from "./assets/book.png";
-import Paradox from "./assets/paradox.png";
+import './App.css';
+import React, { useRef } from 'react';
+import { Canvas, useFrame } from '@react-three/fiber';
+import { Stars } from '@react-three/drei';
+import { motion } from 'framer-motion';
+import Navbar from './pages/Navbar/Navbar';
+import MainBody from './pages/MainBody/MainBody';
+import AiDemo from './pages/AiDemo/AiDemo';
+import Features from './pages/Features/Features';
+import Footer from './pages/Footer/Footer';
+import WorkRoadMap from './pages/WorkRoadmap/WorkRoadMap';
+import BookSample from './pages/BookSample/BookSample';
+import Testimonials from './pages/Testimonials/Testimonials';
+import NewsLetter from './pages/NewsLetter/NewsLetter';
+import Book from './assets/book.png';
+import Paradox from './assets/paradox.png';
 
 function MovingStars() {
   const starsRef = useRef(null);
@@ -31,22 +31,22 @@ function MovingStars() {
 
 function App() {
   return (
-    <div className="App relative">
-      <motion.section className="relative overflow-hidden min-h-screen bg-transparent">
+    <div className='App relative'>
+      <motion.section className='relative overflow-hidden min-h-screen bg-transparent'>
         <Navbar />
         <MainBody />
-        <AiDemo imgSrc={Book} fltImg={Paradox} altText="Ai Book Generator" />
-        <div className="absolute inset-0 -z-10">
+        <AiDemo imgSrc={Book} fltImg={Paradox} altText='Ai Book Generator' />
+        <div className='absolute inset-0 -z-10'>
           <Canvas>
             <MovingStars />
           </Canvas>
         </div>
       </motion.section>
       <Features />
-      <WorkRoadMap heading="How It Works" />
+      <WorkRoadMap heading='How It Works' />
       <BookSample />
-      <AiDemo imgSrc={null} fltImg={null} altText="Ai Book Generator Video" />
-      <WorkRoadMap heading="Roadmap" />
+      <AiDemo imgSrc={null} fltImg={null} altText='Ai Book Generator Video' />
+      <WorkRoadMap heading='Roadmap' />
       <Testimonials />
       <NewsLetter />
       <Footer />
--- frontend/src/pages/Navbar/Navbar.jsx
+++ frontend/src/pages/Navbar/Navbar.jsx
@@ -1,75 +1,50 @@
-import React from "react";
-import { Link } from "react-scroll";
-import Logo from "/logo.png";
-import "./Navbar.css";
+import React from 'react';
+import { Link } from 'react-scroll';
+import Logo from '/logo.png';
+import './Navbar.css';
 
 function Navbar() {
   return (
-    <header className="flex flex-col flex-wrap md:flex-row justify-between">
-      <div className="Navbar m-2">
-        <div className="flex flex-wrap items-center text-2xl font-black">
-          <div className="logoIcon w-8 h-8 rounded-full mr-3 bg-gradient1"></div>
-          <img src={Logo} width={45} alt="Book Ai Logo" />
+    <header className='flex flex-col flex-wrap md:flex-row justify-between'>
+      <div className='Navbar m-2'>
+        <div className='flex flex-wrap items-center text-2xl font-black'>
+          <div className='logoIcon w-8 h-8 rounded-full mr-3 bg-gradient1'></div>
+          <img src={Logo} width={45} alt='Book Ai Logo' />
           BookAi
         </div>
       </div>
 
-      <nav className="flex flex-row items-center flex-wrap font-light">
-        <Link
-          to="features"
-          smooth={true}
-          duration={500}
-          className="m-2 hover:text-accentColor"
-        >
+      <nav className='flex flex-row items-center flex-wrap font-light'>
+        <Link to='features' smooth={true} duration={500} className='m-2 hover:text-accentColor'>
           Features
         </Link>
 
         <Link
-          to="howItWorks"
+          to='howItWorks'
           smooth={true}
           duration={500}
-          className="m-2 hover:text-accentColor block"
+          className='m-2 hover:text-accentColor block'
         >
           How it Works
         </Link>
 
-        <Link
-          to="roadmap"
-          smooth={true}
-          duration={500}
-          className="m-2 hover:text-accentColor"
-        >
+        <Link to='roadmap' smooth={true} duration={500} className='m-2 hover:text-accentColor'>
           Roadmap
         </Link>
 
-        <Link
-          to="api"
-          smooth={true}
-          duration={500}
-          className="m-2 hover:text-accentColor"
-        >
+        <Link to='api' smooth={true} duration={500} className='m-2 hover:text-accentColor'>
           API
         </Link>
 
-        <Link
-          to="price"
-          smooth={true}
-          duration={500}
-          className="m-2 hover:text-accentColor"
-        >
+        <Link to='price' smooth={true} duration={500} className='m-2 hover:text-accentColor'>
           Price
         </Link>
 
-        <Link
-          to="models"
-          smooth={true}
-          duration={500}
-          className="m-2 hover:text-accentColor"
-        >
+        <Link to='models' smooth={true} duration={500} className='m-2 hover:text-accentColor'>
           Models
         </Link>
 
-        <a href="#" className="auth-button bg-gradient1">
+        <a href='#' className='auth-button bg-gradient1'>
           Login / Sign Up
         </a>
       </nav>
